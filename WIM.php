622b309895d7bbe6ebc6da1c54947abe071b7013
googoodoll25 <pt.tharn@gmail.com>
2021-08-09 19:58:08 +0700

    Navbar

---
+++ WIM.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Source/image/favicon.ico" type="image/x-icon">
+    <title>WIM Monitoring</title>
+    <link rel="stylesheet" href="Source/Bootstrap/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="Source/css/css.css">
+</head>
+<body> 
+<nav class="navbar navbar-expand-lg navbar-dark navColor">
+
+    <img src="Source/image/LogoPAT.png" alt="" width="30" height="30" class="d-inline-block align-top">
+    <a class="navbar-brand ml-2" href="#">WIM Monitoring</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse mr-5" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item" id="nav-item">
+         
+        <a class="nav-link mr-3" href="#"> WIM</a>
+      </li>
+      <li class="nav-item" id="nav-item">
+        <a class="nav-link" href="#">VWS</a>
+      </li>  
+    </ul>
+  </div>
+</nav>
+
+
+<script src="Source/Bootstrap/jquery.min.js" crossorigin="anonymous"></script>
+<script src="Source/Bootstrap/popper.min.js" crossorigin="anonymous"></script>
+<script src="Source/Bootstrap/bootstrap.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
